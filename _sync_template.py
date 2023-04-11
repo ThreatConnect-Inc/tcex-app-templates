8e3c63056c59340b56db0c3861bed1a139496f4b
Chris Blades <cblades@threatconnect.com>
2023-04-11 12:03:45 -0400

    update sync script for api service basic template

--- _sync_template.py
+++ _sync_template.py
@@ -22,6 +22,7 @@ class SyncTemplate:
         self.playbook_action_dst_path = Path('playbook/actions/')
         self.playbook_basic_dst_path = Path('playbook/basic/')
         self.playbook_utility_dst_path = Path('playbook/utility/')
+        self.api_service_basic_dst_path = Path('api_service/basic/')
 
     def _copy_file(self, src_file, dst_file):
         """."""
@@ -128,6 +129,43 @@ class SyncTemplate:
                 elif filename in self.app_common_template_files:
                     self._copy_file(file, self.app_common_dst_path / filename)
 
+    def sync_apiservice_basic(self):
+        """."""
+        src_path = self.base_path / 'tcva-tcex-4-basic-template/'
+        dst_path = self.api_service_basic_dst_path
+        for file in src_path.rglob('*'):
+            # only process items at the top level
+            if file.parent != src_path:
+                continue
+
+            if file.is_dir():
+                if file.name.startswith('.'):
+                    continue
+
+                if file.name in ['app_notebook', 'tests']:
+                    shutil.rmtree(dst_path / file.name)
+                    shutil.copytree(file, dst_path / file.name)
+            elif file.is_file():
+                filename = file.name
+                if file.name == '.gitignore':
+                    filename = 'gitignore'
+
+                # send to most specific location first
+                if file.name in [
+                    'app_inputs.json',
+                    'app_inputs.py',
+                    'api_service_app.py',
+                    'app.py',
+                    'install.json',
+                    'README.md',
+                    'requirements.txt',
+                    'run.py',
+                ]:
+                    self._copy_file(file, dst_path / filename)
+                # send to parent location
+                elif file.name in self.app_common_template_files:
+                    self._copy_file(file, self.app_common_dst_path / filename)
+
     def sync_playbook_utility(self):
         """."""
         src_path = self.base_path / 'tcpb-tcex-4-utility-template/'
@@ -188,6 +226,10 @@ def sync(
 ):
     """Sync template files"""
     match template_type:
+        case 'api_service':
+            match template_name:
+                case 'basic':
+                    sync_template.sync_apiservice_basic()
         case 'playbook':
             match template_name:
                 case 'actions':
